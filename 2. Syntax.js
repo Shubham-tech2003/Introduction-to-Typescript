1c5d6d469fdba5c601fd92db942f13413adf74ed
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-21 13:56:35 +0530

    looop

---
+++ 2. Syntax.js
@@ -0,0 +1,10 @@
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        console.log("Hey, Doing this for Office xD");
+    };
+    return Greeting;
+}());
+var obj = new Greeting();
+obj.greet();
