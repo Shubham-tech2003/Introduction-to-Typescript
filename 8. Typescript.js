755c99bd7efe7e4fc4ec9ff1122e67ede7329fdd
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-21 13:57:26 +0530

    typescript.js

---
+++ 8. Typescript.js
@@ -0,0 +1,18 @@
+var customer = {
+    firstname: "Soumyadip",
+    lastname: "Chowdhury",
+    sayHi: function () { return "Hey :)"; }
+};
+console.log("Customer Object ");
+console.log(customer.firstname);
+console.log(customer.lastname);
+console.log(customer.sayHi());
+var customer1 = {
+    firstname: "Soumyadip",
+    lastname: "Chowdhury",
+    sayHi: function () { return "Hey :)"; }
+};
+console.log("Customer1 Object ");
+console.log(customer1.firstname);
+console.log(customer1.lastname);
+console.log(customer1.sayHi());
